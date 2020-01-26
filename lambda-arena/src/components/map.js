66a1309339ce84c8cf85fd01e207b71ba3e04cd5
Ming Liu <skysthelimit68@gmail.com>
2020-01-25 21:40:14 -0500

    generating map / grid, adding room component, remove setstate for rooms (using grid for room tracking), adding active room state, add pulsating animation for active room indicator

--- lambda-arena/src/components/map.js
+++ lambda-arena/src/components/map.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import axios from 'axios';
-import { FormHelperText } from '@material-ui/core';
-
+import Room from './room';
 
 export default class Map extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            rooms : null,
-            active_rm : null,
+            grid: null,
+            active_rm : {"x":null, "y":null},
             min_x : 0,
             max_x : 0,
             min_y : 0,
             max_y : 0,
             width : 0,
             height: 0,
-            temp_rm : {},
-            grid: null
         }
     } 
 
@@ -24,25 +21,17 @@ export default class Map extends React.Component{
         axios
         .get("https://lambdamud-2020-staging.herokuapp.com/api/gameworld/")
         .then(res => {
-            this.setState({
-                rooms : res.data,
-                temp_rm : res.data[0]
-            })
-            console.log(this.state.rooms[3])
-
-            this.setMinMax()
-
-            console.log(this.state.min_x, this.state.max_x, this.state.min_y, this.state.max_y, this.state.width, this.state.height)
-
-            this.generatingMap()
+            this.setMinMax(res.data)
+            //console.log(this.state.min_x, this.state.max_x, this.state.min_y, this.state.max_y, this.state.width, this.state.height)
+            this.generatingMap(res.data)
         })
         .catch(err=>{
             console.log(err)
         })
     }
 
-    setMinMax = () => {
-        this.state.rooms.forEach(room => {
+    setMinMax = (rooms) => {
+       rooms.forEach(room => {
             this.setState({
                 min_x : room.x < this.state.min_x ? room.x : this.state.min_x,
                 max_x : room.x > this.state.max_x ? room.x : this.state.max_x,
@@ -60,7 +49,7 @@ export default class Map extends React.Component{
         })
     }
 
-    generatingMap = () => {
+    generatingMap = (rooms) => {
         let grid = []
         for (let i = 0; i < this.state.height ; i ++){
             grid[i] = []
@@ -71,7 +60,7 @@ export default class Map extends React.Component{
         let x_offset = Math.abs(this.state.min_x)
         let y_offset = Math.abs(this.state.min_y)
 
-        this.state.rooms.forEach(room => {
+        rooms.forEach(room => {
             grid[room.y + y_offset][room.x + x_offset] = room
         })
         this.setState({
@@ -79,68 +68,32 @@ export default class Map extends React.Component{
         })
         console.log(this.state.grid)
     }
-    
-    getStyle = (room) => {
-        let wall = "2px solid #333";
-        let top = "";
-        let bottom = "";
-        let left = ""; 
-        let right = "";
-        if(!room.n_to) top = wall
-        if(!room.s_to) bottom = wall
-        if(!room.e_to) right = wall
-        if(!room.w_to) left = wall
-        return {boxSizing: "border-box",
-                width: "50px",
-                height: "50px",
-                flex: "0 0 50px",
-                fontSize:10,
-                color: "#000000", 
-                background: "#ffffff",
-                borderTop: top,
-                borderBottom: bottom,
-                borderRight: right,
-                borderLeft: left}
-    }
 
+    setActiveRoom = (x, y) => {
+        this.setState({
+            active_room : {"x" : x, "y": y}
+        })
+    }
 
     render() {
         let box_size = this.state.width > this.state.height ? 600 /this.state.width : 600 / this.state.height
-        const style = {
-            box : {
-                boxSizing: "border-box",
-                width: "50px",
-                height: "50px",
-                flex: "0 0 50px",
-                fontSize:10,
-                color: "#000000", 
-                background: "#ffffff",
-            },
-            box_empty : {
-                boxSizing: "border-box",
-                width: "50px",
-                height: "50px",
-                flex: "0 0 50px",
-                fontSize:10,
-                color: "#000000", 
-                background: "#999999"
-            }
-            
-        }
-        return(
+
+        return (
             
 
-        <div style={{width:600, display:"flex", flexWrap: "wrap", margin: "0 auto"}}>{this.state.grid == null ? 
-            "Generating Map" 
-            : 
-            this.state.grid.reverse().map((col, key) => {
-                return col.map((room, key )=> {
-                return room ? <div style={this.getStyle(room)}>{room.rm_id}</div> : <div style={style.box_empty}></div>
+            <div style={{width:600, display:"flex", flexWrap: "wrap", margin: "0 auto"}}>
+                {this.state.grid == null ? 
+                "Generating Map" 
+                : 
+                this.state.grid.reverse().map((col) => {
+                    return col.map((room, key)=> {
+                        const active = room ? room.x == this.state.active_rm["x"] && room.y == this.state.active_rm["y"] ? true : false : false  
+                        return <Room room={room} box_size={box_size} uniq={key} active={active}/>
+                    })
                 })
-
-            })}</div>
+                }
+            </div>
         )
 
-        
     }
 }
---
+++ lambda-arena/src/components/room.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+
+export default class Room extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    getStyle = (room) => {
+        let wall = "2px solid #333";
+        let top = "";
+        let bottom = "";
+        let left = ""; 
+        let right = "";
+        if(!room.n_to) top = wall
+        if(!room.s_to) bottom = wall
+        if(!room.e_to) right = wall
+        if(!room.w_to) left = wall
+        return {boxSizing: "border-box",
+                width: "50px",
+                height: "50px",
+                flex: "0 0 50px",
+                fontSize:10,
+                color: "#000000", 
+                background: "#ffffff",
+                borderTop: top,
+                borderBottom: bottom,
+                borderRight: right,
+                borderLeft: left}
+    }
+
+
+    render(){
+
+        const style = {
+            
+            box_empty : {
+                boxSizing: "border-box",
+                width: "50px",
+                height: "50px",
+                flex: "0 0 50px",
+                fontSize:10,
+                color: "#000000", 
+                background: "#999999"
+            }
+            
+        }
+        return (
+            this.props.room 
+            ? 
+            <div style={this.getStyle(this.props.room)} key={this.props.uniq}>{this.props.active ? <div className="active-room"></div> : this.props.room.rm_id}</div>
+            :
+            <div style={style.box_empty} key={this.props.uniq}></div>
+        )
+    }
+}
