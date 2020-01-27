5fede73cfa7017ca6824de403737ab9c901a4f13
Ming Liu <skysthelimit68@gmail.com>
2020-01-26 20:46:11 -0500

    move game out of header

--- lambda-arena/src/App.js
+++ lambda-arena/src/App.js
@@ -7,9 +7,10 @@ import Login from './components/login';
 function App() {
   return (
     <div className="App" style={{width:'100%'}}>
-      <header className="App-header">
+      {/* <header className="App-header">
+      <h1>Lambda Arena</h1>
+      </header> */}
       <Game style={{width:'100%'}}/>
-      </header>
     </div>
   );
 }
