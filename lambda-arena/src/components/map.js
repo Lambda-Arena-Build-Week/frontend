622f6288123ab68936f1aa7fd5353459b4c7114e
Christopher Riffle <37821010+cmr629@users.noreply.github.com>
2020-01-27 19:51:30 -0500

    Update map.js

--- lambda-arena/src/components/map.js
+++ lambda-arena/src/components/map.js
@@ -16,7 +16,7 @@ export default class Map extends React.Component{
             max_y : 0,
             width : 0,
             height : 0,
-            reverse : false
+            reverse : true
         }
         this.mapLoaded = false;
         this.unityContent = new UnityContent(
